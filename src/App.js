2aa189f9e691e9f77b460f212a08ca1c602eb29e
Beth <bethmelmtv@gmail.com>
2022-05-18 23:56:28 -0700

    worked on table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import PokemonSearch from './PokemonSearch';
 import YelpSearch from './YelpSearch';
 import WeatherSearch from './WeatherSearch';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [yelpData, setYelpData] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
+
+  const [yelpQuery, setYelpQuery] = useState('');
+  const [weatherQuery, setWeatherQuery] = useState('');
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  // async function handleYelpSearch(e) {
+  //   e.preventDefault();
+  //   const response = await getYelpData(yelpQuery);
+  //   setYelpQuery(response);
+  // }
+
+  // async function handleWeatherSearch(e) {
+  //   e.preventDefault();
+  //   const response = await getWeatherData(weatherQuery);
+  //   setWeatherQuery(response);
+  // }
+
   return (
     <div className="App">
       <PokemonSearch />
       <YelpSearch />
-    { /* stretch goal: <WeatherSearch /> */ }
+      <WeatherSearch />
+      {/* stretch goal: <WeatherSearch /> */}
     </div>
   );
 }
--- src/PokemonSearch.js
+++ src/PokemonSearch.js
@@ -1,27 +1,41 @@
 import { useState } from 'react';
 
 export default function PokemonSearch() {
-      // you'll need to track your pokemon search results, the loading state, and one form field: name. For this form field, set a real initial values (like 'pikachu') so the form populates with a default value.
-  
+  // you'll need to track your pokemon search results, the loading state, and one form field: name. For this form field, set a real initial values (like 'pikachu') so the form populates with a default value.
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonQuery, setPokemonQuery] = useState('pokemon');
+  const [loading, setLoading] = useState(true);
+
   async function handlePokemonSubmit(e) {
     e.preventDefault();
-      
-        // set the loading state to true
-        // use fetch to make a request to your netlify pokemon function. Be sure to pass the pokemon name as a query param in the URL
-  
-        // put the jsonified data in state and set the loading state to false
+
+    // set the loading state to true
+    setLoading(true);
+    // use fetch to make a request to your netlify pokemon function. Be sure to pass the pokemon name as a query param in the URL
+    const response = await getPokemonData(pokemonQuery);
+    //getPokemonData is coming from fetch/utils which is our endpoint
+    // put the jsonified data in state and set the loading state to false
+    setPokemonData(response.data.business);
+    setLoading(false);
   }
-      
+
   return (
-    <section className='pokemon'>
+    <section className="pokemon">
       {/* make the fetch on submit */}
-      <form>
-            Search pokemon for a city
+      <form onSubmit={handlePokemonSubmit}>
+        Search pokemon for a city
         {/* add inputs/labels for the pokemon name, using all the things we need with react forms. Don't forget to use the value property to sync this up with the default value in react state */}
+        <input onChange={(e) => setPokemonQuery(e.target.value)} value={pokemonQuery} />
         <button>Get pokemon</button>
       </form>
+
+      <h2>Pokemon List</h2>
+      {pokemonData.map(({ pokemon }, i) => (
+        <div key={pokemon + i}>
+          <h3>{pokemon}</h3>
+        </div>
+      ))}
       {/* Make a PokemonList component to import and use here. Use a ternery to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
     </section>
   );
-
 }
